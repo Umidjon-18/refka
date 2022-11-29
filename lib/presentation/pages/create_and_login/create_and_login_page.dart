bec1a253408b6a42f0228d97d4a3fbd8bbd3f3a5
Umidjon Yoqubov <umidjonxomidjonovich@gmail.com>
2022-11-29 15:55:35 +0500

    ok

---
+++ lib/presentation/pages/create_and_login/create_and_login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CreateAndLoginPage extends StatefulWidget {
+  const CreateAndLoginPage({super.key});
+
+  @override
+  State<CreateAndLoginPage> createState() => _CreateAndLoginPageState();
+}
+
+class _CreateAndLoginPageState extends State<CreateAndLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/routes/routes.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../pages/create_and_login/create_and_login_page.dart';
+
+class Routes {
+  static const createAndLogin = '/';
+  static const createAccount = '/createAccount';
+  static const login = '/login';
+
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+    try {
+      final Map<String, dynamic>? args =
+          routeSettings.arguments as Map<String, dynamic>?;
+      args ?? <String, dynamic>{};
+      switch (routeSettings.name) {
+        case createAndLogin:
+          return MaterialPageRoute(
+            settings: routeSettings,
+            builder: (_) => const CreateAndLoginPage(),
+          );
+        // case createAccount:
+        //   return MaterialPageRoute(
+        //     settings: routeSettings,
+        //     builder: (_) => SignWelcomePage(),
+        //   );
+        // case login:
+        //   return MaterialPageRoute(
+        //     settings: routeSettings,
+        //     builder: (_) => MainPage(),
+        //   );
+        default:
+          return MaterialPageRoute(
+            settings: routeSettings,
+            builder: (_) => const CreateAndLoginPage(),
+          );
+      }
+    } catch (e) {
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const CreateAndLoginPage(),
+      );
+    }
+  }
+}
